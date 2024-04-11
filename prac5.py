b377c9875553b5c69d0bcbd2d0d27e9b90dfe559
minkyeong <sao09101@naver.com>
2024-04-12 01:10:27 +0900

    prac5.py modify

--- prac5.py
+++ prac5.py
@@ -2,17 +2,31 @@
 
 class Zoo:
     def __init__(self):
-        pass
+        self.animal_list = []
+        
 
     def add_animal(self, animal):
-        pass
+        for exist_animal in self.animal_list:
+            if (exist_animal.name == animal.name) and (exist_animal.species == animal.species) : 
+                print(f'"{animal.name} the {animal.species}" 은 이미 동물원에 있습니다. 다른 이름을 사용하거나, 다른 종으로 추가해주세요.')
+                pass 
+        self.animal_list.append(animal)
 
     def show_animals(self):
-        pass
+        print('현재 동물원에는 다음 동물들이 있습니다: ')
+        for animal in self.animal_list : 
+            print(f' - {animal.name} the {animal.species}')
+        
+    def show_animals_by_species(self , species) :
+        for animal in self.animal_list:
+            if animal.species == species :
+                print(f' - {animal.name} the {animal.species}')
 
 class Animal:
     def __init__(self, name, species):
-        pass
+        self.name = name
+        self.species = species
+        
 
 def main():
     # 동물원을 선언하고 10마리의 동물들을 추가합니다.
